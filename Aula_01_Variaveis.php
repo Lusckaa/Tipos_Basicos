f7de7392854bce80882dc7c33164d58a8ab35a9f
Darthx <lucasviana.discente@fha.mg.gov.br>
2022-10-30 17:39:06 -0300

    initial commit

---
+++ Aula_01_Variaveis.php
@@ -0,0 +1,27 @@
+<?php
+
+// Strings
+
+$nome = "Edilson";
+echo $nome, "<br>";
+
+// Número inteiro
+
+$numeroInteiro = 15;
+echo $numeroInteiro, "<br>";
+
+// Número do ponto flutuante
+
+$numeroDecimal = 2.5;
+echo $numeroDecimal, "<br>";
+
+// Booleans
+
+$information1 = true;
+echo $information1, "<br>";
+
+$information2 = false;
+echo $information2, "<br>";
+
+
+
